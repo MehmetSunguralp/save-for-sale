200cf2e4b35827edc8d57283892963fdc019af09
Mehmet Alp <mehmetsunguralp54@gmail.com>
2024-11-24 22:22:12 +0300

    Domain checking on the client implemented

--- client/src/components/AdWrapper/AdWrapper.jsx
+++ client/src/components/AdWrapper/AdWrapper.jsx
@@ -1,6 +1,6 @@
-import { useEffect } from "react";
 import { createAd, refreshAds } from "../../utils/api";
 import "./AdWrapper.css";
+import mainLogo from "/main-logo.png";
 
 import React from "react";
 import { AdItem } from "../AdItem/AdItem";
@@ -9,18 +9,38 @@ export const AdWrapper = ({ list }) => {
 	// const currentDomainPath = window.location.href;
 	//const currentDomainPath = "https://www.sahibinden.com/ilan/ozel-ders-verenler-yabanci-dil-almanca-dersi-1153549516/detay/";
 	//const currentDomainPath = "https://www.emlakjet.com/ilan/ayrancilar-ege-kent-4de-31-kiralik-daire-cagri-emlaktan-10245254/";
-	const currentDomainPath = "https://www.hepsiemlak.com/ankara-polatli-mehmet-akif-kiralik/daire/151732-10";
+	const currentDomainPath = "https://www.hepsiemlak.com/ankara-polatli-fatih-kiralik/daire/151732-1";
 	//const currentDomainPath = "https://www.arabam.com/ilan/galeriden-satilik-fiat-doblo-combi-1-3-multijet-safeline/2019-model-doblo-combi-1-3-multijet-safeline/26933812";
 	//const currentDomainPath = "https://www.letgo.com/item/bugaboo-bee-bebek-arabas-iid-1694381927";
 
 	const handleDomain = () => {
-		createAd(currentDomainPath);
+		let isInstanceOf = false;
+		const existingList = JSON.parse(localStorage.getItem("adList"));
+		existingList.some((ad) => {
+			if (ad.url == currentDomainPath) {
+				isInstanceOf = true;
+				return true;
+			}
+		});
+		if (isInstanceOf) {
+			alert("URL already exists: " + currentDomainPath);
+		} else if (
+			currentDomainPath.includes("https://www.sahibinden.com/ilan") ||
+			currentDomainPath.includes("https://www.emlakjet.com/ilan") ||
+			currentDomainPath.includes("https://www.hepsiemlak.com") ||
+			currentDomainPath.includes("https://www.arabam.com/ilan") ||
+			(currentDomainPath.includes("https://www.letgo.com/item") && isInstanceOf == false)
+		) {
+			createAd(currentDomainPath);
+		} else {
+			alert("Invalid domain!");
+		}
 	};
 
 	return (
 		<div>
 			<div className="header">
-				<label className="main-title">Save-For-Sale</label>
+				<img src={mainLogo} alt="main-logo" className="main-logo" />
 				<button className="add-btn" onClick={handleDomain}>
 					İlan Ekle
 				</button>
